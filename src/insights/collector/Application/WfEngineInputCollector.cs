d8b7ee3228aa4c9ba534d75ed2d8f812b4ec30e6
medbalighhamdi <mohamed.baligh.hamdi@gmail.com>
2026-02-13 15:27:36 +0100

    core: adding dateonly formatter for date only management core: adding aliases for properties dut to camel case formatting issues

--- src/insights/collector/Application/WfEngineInputCollector.cs
+++ src/insights/collector/Application/WfEngineInputCollector.cs
@@ -25,8 +25,7 @@ public class WfEngineInputCollector(
     {
         var engineCallConfig = masterDataServiceConfig.Value.Parameters;
         var wfActuals =  (await masterDataClient.GetHistoricalSlices(
-            referenceDate, 
-            1,
+            referenceDate,
             new HistorySearchParams(engineCallConfig.YearsHistoryDepth, engineCallConfig.RollingWindowDays, engineCallConfig.LeapDayStrategy), cancellationToken)).ToList();
         await insightMonitor.ApplyValidationAndLogs(referenceDate, engineCallConfig, wfActuals, cancellationToken);
         var cityIds = (await masterDataClient.GetAllCities()).Select(c => c.Id).ToHashSet();
--- src/insights/collector/Presentation/Extensions/ServiceCollectionExtensions.cs
+++ src/insights/collector/Presentation/Extensions/ServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@ using WeatherInsights.Collector.Application;
 using WeatherInsights.Collector.Application.Interfaces;
 using WeatherInsights.Collector.Domain.Ports.Clients.Interfaces;
 using WeatherInsights.Collector.Domain.Ports.Config;
-using WeatherInsights.Collector.Domain.Ports.Repositories;
 using WeatherInsights.Collector.Domain.Ports.Repositories.Interfaces;
 using WeatherInsights.Collector.Infrastructure.Clients;
 using WeatherInsights.Collector.Infrastructure.Connectors;

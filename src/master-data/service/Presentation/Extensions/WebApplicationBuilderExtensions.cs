6e9865334e3f6ac2c488675e2ac2ffafb8a7c3ca
medbalighhamdi <mohamed.baligh.hamdi@gmail.com>
2026-02-25 01:21:43 +0100

    cleanup: master data: remove unnecessary config

--- src/master-data/service/Presentation/Extensions/WebApplicationBuilderExtensions.cs
+++ src/master-data/service/Presentation/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,5 +1,3 @@
-using MasterData.Infrastructure.Config;
-
 namespace MasterData.Api.Extensions;
 
 public static class WebApplicationBuilderExtensions
@@ -8,7 +6,5 @@ public static class WebApplicationBuilderExtensions
     {
         webApplicationBuilder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
         webApplicationBuilder.Configuration.AddEnvironmentVariables();
-        webApplicationBuilder.Services.Configure<MasterDataServiceConfig>(
-            webApplicationBuilder.Configuration.GetSection("MasterDataDb"));
     }
 }
